1cbadbb66771977ed0d3ecbca29818a6f16e7126
Jason Morgan <jason.morgan@acceleration.biz>
2020-12-16 23:47:06 -0500

    updated test

--- test/auto/index.ts
+++ test/auto/index.ts
@@ -4,21 +4,17 @@ const should = chai.should();
 
 import { ApiKeyAuth, CreateListRequestModel, ListApi, ListApiApiKeys } from "../../src/selligent/api";
 
-describe("Create Lists", ()=>{
+describe("Get List", ()=>{
 	it("should create list", async ()=>{
 		const client = new ListApi("https://teachco.slgnt.us/Portal/Api")
 		const auth = new ApiKeyAuth("header", "X-ApiKey");
 		auth.apiKey = [process.env.SELLIGENT_API_KEY, process.env.SELLIGENT_API_SECRET].join(":");
 		client.setDefaultAuthentication(auth)
 		
-		return client.listCreateList("teachco_do_not_use_accel", {
-			"apiName": "my_api_generated_list",
-			"description": "my_api_generated_list",
-			"name": "my_api_generated_list",
-			"type": CreateListRequestModel.TypeEnum.Datalist
-		}).then((response)=>{
-			should.exist(response.body.message)
-			console.log(response);
+		return client.listGetList("my_api_generated_list", "teachco_do_not_use_accel").then((response)=>{
+			console.log(response.body);
+			response.body.name.should.eq("my_api_generated_list");
+			
 			return "";
 		}).catch((error)=>{
 			console.log(error);
