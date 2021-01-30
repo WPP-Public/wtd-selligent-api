43f1b9304cb8cf82be4da51c71113e61146d1efe
Jason Morgan <jason.morgan@acceleration.biz>
2021-01-30 00:06:26 -0500

    fixed list api

--- test/auto/index.ts
+++ test/auto/index.ts
@@ -11,14 +11,14 @@ describe("Get List", ()=>{
 		auth.apiKey = [process.env.SELLIGENT_API_KEY, process.env.SELLIGENT_API_SECRET].join(":");
 		client.setDefaultAuthentication(auth);
 		return client.listGetLists("tgc-direct_prod", "Userlist", "TGCD_CONTACTS_LIST")
-		.then((res)=>{
-			console.log(res.body);
-			should.exist(res.body.lists, "List");
-			return "";
-		}).catch((error)=>{
-			console.log(error);
-			should.not.exist(error, "Error");
-			return ""
-		});
+			.then((res)=>{
+				console.log(res.body);
+				should.exist(res.body.lists, "List");
+				return "";
+			}).catch((error)=>{
+				console.log(error);
+				should.not.exist(error, "Error");
+				return ""
+			});
 	});
 });
